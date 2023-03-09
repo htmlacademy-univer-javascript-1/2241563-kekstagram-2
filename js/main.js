9c0b249bdf5e6902c796a3d43ebbf53a7990ae21
Maksimyska <103052509+Maksimyska@users.noreply.github.com>
2023-03-09 23:32:02 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-function truncate(str, maxlength) {
-  return (str.length > maxlength) ?
-    str.slice(0, maxlength - 1) + '…' : str;
+function getRandomInt(from, to){
+  if (from < 0 || to < 0){
+    throw new RangeError('Числа в диапазоне должны быть положительными');
+  }
+
+  if (from === to) {
+    return from;
+  }
+  if (from > to){
+    [from, to] = [to, from];
+  }
+
+  return Math.round(Math.random() * (to - from) + from);
 }
+
+const isCorrectLength = (str, maxLength) => str.length <= maxLength;
+
+export {getRandomInt, isCorrectLength}; // для того чтобы линтер не ругался
