6e5e1bef1873ce099e7d17f428129f871e373b93
Maks <max.swimEKB@bk.ru>
2023-03-10 21:09:20 +0500

    modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-function truncate(str, maxlength) {
-  return (str.length > maxlength) ?
-    str.slice(0, maxlength - 1) + '…' : str;
-}
 import {generateDescription, PHOTOS_COUNT} from './data';
 import {isCorrectLength} from './util';
 
