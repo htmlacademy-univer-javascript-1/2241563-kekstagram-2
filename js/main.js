db671311a4587a56cba0b611d12cc2d42030d3d2
Maks <max.swimEKB@bk.ru>
2023-03-10 17:01:51 +0500

    modified: js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ function truncate(str, maxlength) {
   return (str.length > maxlength) ?
     str.slice(0, maxlength - 1) + '…' : str;
 }
+import {generateDescription, PHOTOS_COUNT} from './data';
+import {isCorrectLength} from './util';
+
+const descriptions = Array.from({length: PHOTOS_COUNT}, generateDescription);
+
+isCorrectLength(descriptions, PHOTOS_COUNT); // Чтобы eslint не ругался
