838fbdc6181044aa7e023baf78c04804d5a7c563
maksik <max.swimEKB@bk.ru>
2023-03-12 00:34:59 +0500

    feat: inserting descriptions into the DOM

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-import {generateDescription, PHOTOS_COUNT} from './data';
-import {isCorrectLength} from './util';
+import {generateDescription, PHOTOS_COUNT} from './data.js';
+import {isCorrectLength} from './util.js';
+import {insertDescriptions} from './user.js';
 
 const descriptions = Array.from({length: PHOTOS_COUNT}, generateDescription);
+insertDescriptions(descriptions);
 
 isCorrectLength(descriptions, PHOTOS_COUNT); // Чтобы eslint не ругался
