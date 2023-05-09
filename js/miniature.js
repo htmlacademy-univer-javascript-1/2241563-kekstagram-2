b80d173207377ef4c2ec6247a594762f9f6f4b62
Maksimyska <103052509+Maksimyska@users.noreply.github.com>
2023-05-09 15:20:08 +0500

    Revert "Отрисуй меня полностью (часть 2)"

--- js/miniature.js
+++ js/miniature.js
@@ -1,20 +1,15 @@
-import { showBigPicture } from './fullphoto.js';
-
 const pictureTemplate = document.querySelector('#picture');
 const pictureContainer = document.querySelector('.pictures');
 
 export function insertPhotoMiniature(descriptions) {
   const pictureFragment = document.createDocumentFragment();
-  for (const desc of descriptions) {
+  for (const { url, likes, comments } of descriptions) {
     const picture = pictureTemplate.cloneNode(true).content;
-    picture.querySelector('.picture__img').src = desc.url;
-    picture.querySelector('.picture__img').addEventListener('click', (ev) => {
-      ev.preventDefault();
-      showBigPicture(desc);
-    });
-    picture.querySelector('.picture__likes').textContent = desc.likes;
-    picture.querySelector('.picture__comments').textContent = desc.comments.length;
+    picture.querySelector('.picture__img').src = url;
+    picture.querySelector('.picture__likes').textContent = likes;
+    picture.querySelector('.picture__comments').textContent = comments.length;
     pictureFragment.appendChild(picture);
   }
+
   pictureContainer.appendChild(pictureFragment);
 }
